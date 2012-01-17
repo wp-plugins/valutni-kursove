6c177a3416263035bfb496abeaed7520f719cca2
mlazarov <mlazarov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-17 21:17:56 +0000

    Adding first version of Valutni Kursove plugin
    git-svn-id: https://plugins.svn.wordpress.org/valutni-kursove/trunk@491402 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,37 @@
+=== Valutni Kursove ===
+Tags: valuta, kurs, levove, dolari, evro, bgn, eur, lev, usd, валутни курсове, валутен курс
+Requires at least: 2.4
+Tested up to: 3.3.0
+Stable tag: 1.0.0
+Donate link: http://marto.lazarov.org/plugins/valutni-kursove
+Contributors: mlazarov
+
+Valutni курсове ви дава възможност лесно да изведете във вашия блог информация за валутните курсове за деня по информация от БНБ.
+
+== Description ==
+
+Valutni курсове ви дава възможност лесно да изведете във вашия блог информация за валутните курсове за деня по информация от БНБ.
+
+Внимание: валутните курсове се извличат от посреднически източник, за да не се товари излишно сайта на БНБ, като данните са предварително взети от сайта на БНБ и прехвърлени в подходящ формат.
+
+== Frequently Asked Questions ==
+
+**How can I submit a bug?**
+
+Write me to lazarov@mail.bg, please, it's the quicker way to have it fixed. You can write to the 
+WordPress forum, too, but I read it rarely.
+
+**Other FAQs?**
+
+I'm collection tips and FAQ on [this page](http://marto.lazarov.org/plugins/valutni-kursove/) 
+so I can update it more easly.
+
+== Changelog ==
+
+= 1.0.0 =
+* First release
+
+== Screenshots ==
+
+1. screenshot-01.png
+
---
+++ valutni-kursove.php
@@ -0,0 +1,93 @@
+<?php
+/*
+Plugin Name: Валутни Курсове
+Plugin Script: valutni-kursove.php
+Plugin URI: http://marto.lazarov.org/plugins/valutni-kursove
+Description: Easiest way to show exchange rates of the Bulgarian National Bank
+Version: 1.0.0
+Author: mlazarov
+Author URI: http://marto.lazarov.org/
+*/
+
+if (class_exists('WP_Widget')) {
+	class Valutni_Kursove_Widget extends WP_Widget {
+		var $plugin;
+		var $plugin_url;
+		var $settings;
+		var $currencies = array('AUD', 'BRL', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK',
+								'GBP', 'HKD', 'HRK', 'HUF', 'IDR', 'ILS', 'INR',
+								'JPY', 'KRW', 'LTL', 'LVL', 'MXN', 'MYR', 'NOK',
+								'NZD', 'PHP', 'PLN', 'RON', 'RUB', 'SEK', 'SGD',
+								'THB', 'TRY', 'USD', 'XAU', 'ZAR');
+		function Valutni_Kursove_Widget(){
+			$widget_ops = array(
+							'classname' => 'widget_valutni_kursove',
+							'description' => 'Show exchange rates of the Bulgarian National Bank. Изведете лесно валутните курсове на БНБ във вашия блог' );
+			$this->WP_Widget('valutni_kursove', '', $widget_ops);
+			$this->settings = $this->get_settings();
+			if(!$this->settings['updated'] || $this->settings['updated']<time()-3600) $this->getFreshData();
+		}
+
+		function update($new_instance, $old_instance) {
+			$instance = $old_instance;
+			$instance['title'] = strip_tags($new_instance['title']);
+			$instance['currencies'] = (array)$new_instance['currencies'];
+			return $instance;
+		}
+		function form($instance) {
+			$plugin = get_plugin_data( __FILE__ );
+			$instance = wp_parse_args( (array) $instance, array( 'title' => 'Валутни курсове', 'width' => '300', 'height' => '400' ) );
+			$title = strip_tags($instance['title']);
+			$currencies = (array)$instance['currencies'];
+			?>
+			<p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
+			<p><label for="<?php echo $this->get_field_id('currencies'); ?>">Select currencies:<br/>
+					<?
+					foreach($this->currencies as $currency){
+						echo '<input type="checkbox" name="'.$this->get_field_name('currencies').'[]" value="'.$currency.'"'.(in_array($currency,$currencies)?' checked="checked"':'').'/> '.$currency."<br/>";
+					}?>
+			 	</label>
+			</p>
+			<?php
+		}
+		function register(){
+			// Get Fresh data onregister
+			$this->getFreshData();
+		}
+		function widget($args, $instance) {
+			$title = empty($instance['title']) ? '&nbsp;' : apply_filters('widget_title', $instance['title']);
+			$currencies = (array)$instance['currencies'];
+			echo $args['before_widget'];
+			if ( !empty( $title ) ) { echo $args['before_title'] . $title . $args['after_title']; };
+			echo '<table border="0">';
+			foreach($currencies as $currency){
+				echo '<tr><td>'.$this->settings['rates']->{$currency}->ratio.'</td><td> '.$currency.'</td><td> = </td><td> '.$this->settings['rates']->{$currency}->rate.' лв</td></tr>';
+			}
+			echo '</table>';
+
+			echo $args['after_widget'];
+		}
+		function getFreshData(){
+			if(function_exists('get_plugin_data'))
+				$plugin = get_plugin_data( __FILE__ );
+			else
+				$plugin['Version'] = 'unk';
+			$json = file_get_contents('http://cdn.wms-tools.com/bnb.php?v='.$plugin['Version']);
+			$data = json_decode($json);
+
+			$this->settings['rates'] = $data;
+			$this->settings['currencies'] = array_keys((array)$data);
+			$this->settings['updated'] = time();
+
+			$this->save_settings($this->settings);
+		}
+	}
+
+	function Valutni_Kursove_Init() {
+		register_widget('Valutni_Kursove_Widget');
+	}
+
+	add_action('widgets_init', 'Valutni_Kursove_Init');
+
+}
+?>
